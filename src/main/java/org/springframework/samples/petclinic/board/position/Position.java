7e826c4603ef4fb5e304e458ebe923437e0d6a20
Andrés Domínguez Ruiz <andresdominguezruiz.ad@gmail.com>
2022-11-05 15:11:11 +0100

    e

--- src/main/java/org/springframework/samples/petclinic/board/position/Position.java
+++ src/main/java/org/springframework/samples/petclinic/board/position/Position.java
@@ -1,5 +1,7 @@
 package org.springframework.samples.petclinic.board.position;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
@@ -42,6 +45,9 @@ public class Position{
     static void print(String s){
         System.out.println(s);
     }
+
+    @OneToMany
+    private List<Position> adjacents;
     
 
     @Override
