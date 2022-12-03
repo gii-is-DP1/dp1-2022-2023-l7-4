49aa8aff3201852653db645b8429639f04b007c3
motero2k <manolo.ob@hotmail.com>
2022-12-03 02:30:13 +0100

    organize more imports

--- src/main/java/org/springframework/samples/petclinic/map/position/PositionController.java
+++ src/main/java/org/springframework/samples/petclinic/map/position/PositionController.java
@@ -3,7 +3,6 @@ package org.springframework.samples.petclinic.map.position;
 import java.util.List;
 
 import javax.validation.Valid;
-import javax.validation.constraints.Positive;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
