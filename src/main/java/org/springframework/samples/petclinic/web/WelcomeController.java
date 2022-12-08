43098669555c6858ac01a4973a7df1f9f1198218
JaviFdez7 <javidelatorre400@gmail.com>
2022-12-08 17:54:42 +0100

    Home menu y style ordenados

--- src/main/java/org/springframework/samples/petclinic/web/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/web/WelcomeController.java
@@ -1,28 +1,17 @@
 package org.springframework.samples.petclinic.web;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.samples.petclinic.model.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class WelcomeController {
 	
-	
-	  @GetMapping({"/","/welcome"})
-	  public String welcome(Map<String, Object> model) {	 
-		
-		List<Person> persons=new ArrayList<Person>();
-		Person person=new Person();
-		person.setFirstName("Manuel ");
-		person.setLastName("Otero");
-		persons.add(person);
-		model.put("persons" , persons);
-		model.put("title", "Tiranos del underdark pero no hay quien entienda spring");
-		model.put("group", "Chiefs");
-	    return "welcome";
-	  }
+		private final String HOMEMENU="home/homeMenu";
+
+		@GetMapping("")
+		public ModelAndView showHomeMenu(){
+			ModelAndView result=new ModelAndView(HOMEMENU);
+			return result;
+		}
 }
