284782c5c92687fe11e40c1aa4c830d87c8038b4
Javier García Aguilar <javiergarciaaguilar2002@gmail.com>
2023-01-08 16:47:42 +0100

    Test creación de partida

--- src/test/java/org/springframework/samples/tyrantsOfTheUnderdark/game/GameServiceTest.java
+++ src/test/java/org/springframework/samples/tyrantsOfTheUnderdark/game/GameServiceTest.java
@@ -10,6 +10,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.samples.tyrantsOfTheUnderdark.board.map.MapTemplate;
+import org.springframework.samples.tyrantsOfTheUnderdark.card.HalfDeck;
 import org.springframework.samples.tyrantsOfTheUnderdark.game.Game;
 import org.springframework.samples.tyrantsOfTheUnderdark.game.GameService;
 import org.springframework.stereotype.Service;
@@ -28,7 +30,27 @@ public class GameServiceTest {
         assertThat(gamesWithName.size()).isEqualTo(1);
     }
 
-
+    @Test
+    public void testCreateGame() {
+        Game game = new Game();
+        game.setName("Partida Nueva");
+        gameService.addPlayerByUsername(game, "anddomrui");
+        gameService.addPlayerByUsername(game, "daviddhc");
+        MapTemplate mapTemplate = new MapTemplate();
+        mapTemplate.setId(1);
+        game.setMapTemplate(mapTemplate);
+        HalfDeck firstHalfDeck = new HalfDeck();
+        firstHalfDeck.setName("Dragons");
+        HalfDeck secondHalfDeck = new HalfDeck();
+        secondHalfDeck.setName("Drow");
+        game.setFirstHalfDeck(firstHalfDeck);
+        game.setSecondHalfDeck(secondHalfDeck);
+        assertThat(game.getSize()).isEqualTo(2);
+        assertThat(game.getName()).isEqualTo("Partida Nueva");
+        assertThat(game.getFirstHalfDeck().getName()).isEqualTo("Dragons");
+        assertThat(game.getSecondHalfDeck().getName()).isEqualTo("Drow");
+        assertThat(game.getMapTemplate().getId()).isEqualTo(1);
+    }
 
     
 }
