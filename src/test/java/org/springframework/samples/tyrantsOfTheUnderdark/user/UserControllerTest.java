6321d05a436c3bb7136d5d0125857e6839390071
andresdominguezruiz <andresdominguezruiz.ad@gmail.com>
2023-01-08 21:15:11 +0100

    test h24

--- src/test/java/org/springframework/samples/tyrantsOfTheUnderdark/user/UserControllerTest.java
+++ src/test/java/org/springframework/samples/tyrantsOfTheUnderdark/user/UserControllerTest.java
@@ -1,13 +1,47 @@
 package org.springframework.samples.tyrantsOfTheUnderdark.user;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext
 public class UserControllerTest {
+    @Autowired
+    private WebApplicationContext context;
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserService userService;
+
+
+    @BeforeEach
+    public void setup(){
+        mockMvc=MockMvcBuilders.webAppContextSetup(context)
+        .apply(SecurityMockMvcConfigurers.springSecurity()).build();
+    }
+
+    @WithMockUser(username="admin2",authorities = {"admin"})
+    @Test
+    public void shouldDeleteAUserThatIsNotPlayingAnyGame() throws Exception{
+        User deletableUser=this.userService.getUserByUsername("javfercas3");
+        mockMvc.perform(get("/users/{username}/delete",deletableUser.getUsername())).andExpect(status().is3xxRedirection())
+        .andExpect(view().name("redirect:/users/list"));
+    }
     
 }
