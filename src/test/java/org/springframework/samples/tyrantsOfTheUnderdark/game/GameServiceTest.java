218c8d801484d70037e16874a9862b40e02c1a7b
Javier García Aguilar <javiergarciaaguilar2002@gmail.com>
2023-01-10 20:10:20 +0100

    Movidos los tests de compra en el mecado

--- src/test/java/org/springframework/samples/tyrantsOfTheUnderdark/game/GameServiceTest.java
+++ src/test/java/org/springframework/samples/tyrantsOfTheUnderdark/game/GameServiceTest.java
@@ -1,11 +1,8 @@
 package org.springframework.samples.tyrantsOfTheUnderdark.game;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.util.ArrayList;
 import java.util.List;
-
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,11 +10,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.samples.tyrantsOfTheUnderdark.card.Card;
-import org.springframework.samples.tyrantsOfTheUnderdark.card.CardService;
-import org.springframework.samples.tyrantsOfTheUnderdark.cardsMovement.MarketPlayerMoveCardsService;
-import org.springframework.samples.tyrantsOfTheUnderdark.player.Player;
-import org.springframework.samples.tyrantsOfTheUnderdark.player.PlayerService;
 import org.springframework.stereotype.Service;
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
@@ -26,16 +18,6 @@ public class GameServiceTest {
 
     @Autowired
     protected GameService gameService;
-
-    @Autowired
-    protected PlayerService playerService;
-
-    @Autowired
-    protected MarketPlayerMoveCardsService marketPlayerMoveCardsService;
-
-    @Autowired
-    protected CardService cardService;
-
 
     @Test
     public void testFindGamesByExistingName(){
@@ -52,30 +34,5 @@ public class GameServiceTest {
         assertThat(game.getSecondHalfDeck().getName()).isEqualTo("Dragons");
         assertThat(game.getMapTemplate().getId()).isEqualTo(1);
     }
-
-    @Test
-    public void testBuyInMarket() throws Exception {
-        Game game = gameService.getGameById(1);
-        Player player = playerService.getPlayerById(1);
-        player.setInfluence(2);
-        List<Card> sellzone = new ArrayList<>(cardService.getAllCards()).subList(3, 8);
-        game.setSellZone(sellzone);
-        Card card = game.getSellZone().get(4);
-        marketPlayerMoveCardsService.buyCard(card, player);
-        assertThat(player.getInfluence()).isEqualTo(0);
-        assertThat(player.getDiscarded().get(player.getDiscarded().size()-1).getId()).isEqualTo(7);        
-    }
-
-    @Test
-    public void testBuyCardWithoutInfluence() throws Exception {
-        Game game = gameService.getGameById(1);
-        Player player = playerService.getPlayerById(1);
-        player.setInfluence(0);
-        List<Card> sellzone = new ArrayList<>(cardService.getAllCards()).subList(3, 8);
-        game.setSellZone(sellzone);
-        Card card = game.getSellZone().get(4);
-        assertThrows(Exception.class,() -> marketPlayerMoveCardsService.buyCard(card, player));
-    }
-
     
 }
