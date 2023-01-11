f204e9041eeda19a72b2c3b64fc88abb9ae46c1f
David <davidzarandieta@gmail.com>
2023-01-11 01:45:45 +0100

    listado de partidas del usuario

--- src/main/java/org/springframework/samples/tyrantsOfTheUnderdark/user/AuthoritiesRepository.java
+++ src/main/java/org/springframework/samples/tyrantsOfTheUnderdark/user/AuthoritiesRepository.java
@@ -1,9 +1,16 @@
 package org.springframework.samples.tyrantsOfTheUnderdark.user;
 
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 
 
 public interface AuthoritiesRepository extends  CrudRepository<Authorities, String>{
+
+    @Query("SELECT auth FROM Authorities auth WHERE auth.authority = ?1")
+    public List<Authorities> findAuthoritiesByName(String name);
 	
 }
